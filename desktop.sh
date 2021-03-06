6dab1412d32b68a7c507496eac97ff25623a596d
awsandy <awsandy@amazon.co.uk>
2021-03-06 12:26:20 +0000

    up

--- desktop.sh
+++ desktop.sh
@@ -4,11 +4,18 @@ sudo apt-get install xubuntu-desktop -qq
 sudo apt-get install xrdp -qq
 
 sudo adduser xrdp ssl-cert  
-# edit /etc/xrdp/startwm
+# edit /etc/xrdp/startwm.sh
 # replace /etc/X11/Xsession lines with
 # startxfce4
+cp startwm.sh /etc/xrdp/startwm.sh
 sudo systemctl restart xrdp
 sudo ufw allow 3389
 
-passwd ubuntu
+
+passwd ubuntu << EOF
+linuxpassword0321
+linuxpassword0321
+EOF
+
+
 
