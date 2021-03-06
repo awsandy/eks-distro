37ae1ce1ac3b810eff730032330fc81be0a6a4f2
awsandy <awsandy@amazon.co.uk>
2021-03-06 09:57:54 +0000

    up

--- install01-x86.sh
+++ install01-x86.sh
@@ -82,42 +82,6 @@ lxc ls
 
 
 
-cat <<EOF> inventory.yaml
-groups:
-  - name: eksd
-    targets:      
-EOF
-for i in `lxc list | grep eth0 | awk '{print $6}'`;do
-echo "      - $i" >> inventory.yaml
-done
-cat <<EOF>> inventory.yaml 
-    config:
-      transport: ssh     
-EOF
-cat inventory.yaml
-cp inventory.yaml ~/.puppetlabs/bolt
-cat <<EOF> eks.sh
-#!/bin/bash
-sudo apt update -y
-sudo snap remove lxd --purge
-sudo snap install eks --channel=latest/edge --classic
-sudo eks start
-sudo eks status
-EOF
-chmod 755 eks.sh
-bolt script run ./eks.sh --user ubuntu --targets all --no-host-key-check
-bolt command run "sudo eks status" --user ubuntu --targets all --no-host-key-check
-
-lxc exec eksd1 -- sudo eks add-node
-# returns a join command - run this on eksd2 eksd3 etc ....
-ssh ubuntu@10.169.147.134 "sudo eks join 10.169.147.183:25000/1828a4b96effd7b3820faef6a70713e7"
-ssh ubuntu@10.169.147.183 "sudo eks config" > .kube/config
-kubectl get nodes
-kubectl cluster-info
-kubectl get all -A
-
-
-
 
 
 
