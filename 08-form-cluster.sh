530357d5a8ae8787417ff36a6f6ffb020a45be93
awsandy <awsandy@amazon.co.uk>
2021-03-12 17:23:56 +0000

    up

--- 08-form-cluster.sh
+++ 08-form-cluster.sh
@@ -1,5 +1,7 @@
+node1=`lxc list | grep eth0 | grep eksd1 | awk '{print $6}'`
 for i in `lxc list | grep eth0 | grep -v eksd1 | awk '{print $6}'`;do
-jcmd=$(ssh ubuntu@eksd1 "sudo eks add-node" | grep eks | head -1)
+jcmd=$(ssh ubuntu@$node1 "sudo eks add-node" | grep eks | head -1)
+echo "joining $i to eksd1"
 ssh ubuntu@$i "sudo $jcmd" 2> /dev/null
 done
 date
