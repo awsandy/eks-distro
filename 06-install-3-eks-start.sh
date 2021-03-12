87f6d005c82951600bfe94b0e53a9332f5128c6e
awsandy <awsandy@amazon.co.uk>
2021-03-12 17:14:47 +0000

    up

--- 06-install-3-eks-start.sh
+++ 06-install-3-eks-start.sh
@@ -1,8 +1,8 @@
 cd ~/eks-distro
 date
 for i in `lxc list | grep eth0 | awk '{print $6}'`;do
-echo "setting up EKS Distro on $i"
+echo "Starting EKS on $i"
 ssh ubuntu@$i "sudo eks start" 2> /dev/null
-echo "EKS Distro completed on $i"
+echo "EKS started on $i"
 date
 done
--- 07-cluster-status.sh
+++ 07-cluster-status.sh
@@ -4,5 +4,7 @@ if [ $? -eq 0 ];then
     echo "ERROR: eks not running on $i sleep and try again..."
     sleep 60  
     ssh ubuntu@$i "sudo eks status" | grep "eks is not running" 2> /dev/null
+else
+ echo "EKS ok on $i"
 fi
 done
